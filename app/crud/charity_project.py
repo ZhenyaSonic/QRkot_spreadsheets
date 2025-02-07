d29827dcc4d8e1e6dce2baa715a373aeb3847368
Евгений Братанов <sonik.zhenyaa@gmail.com>
2025-02-07 23:29:12 +0300

    Two

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,6 +1,6 @@
-from typing import Type, Optional
+from typing import Type, Optional, List
 
-from sqlalchemy import select, asc
+from sqlalchemy import select, asc, extract
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
@@ -37,6 +37,32 @@ class CRUDCharityProject(CRUDBase):
             ).order_by(asc(model.create_date)).limit(1)
         )
         return oldest_open_item.scalars().first()
+
+
+async def get_projects_by_completion_rate(
+        self,
+        session: AsyncSession,) -> List[CharityProject]:
+    """Запрос по завершённым проектам."""
+
+    projects = await session.execute(
+        select([
+            CharityProject.name,
+            CharityProject.description,
+            CharityProject.create_date,
+            CharityProject.close_date]).where(
+            CharityProject.fully_invested.is_(True)
+        ).order_by(
+            extract('year', CharityProject.close_date) -
+            extract('year', CharityProject.create_date),
+            extract('month', CharityProject.close_date) -
+            extract('month', CharityProject.create_date),
+            extract('day', CharityProject.close_date) -
+            extract('day', CharityProject.create_date),
+            extract('hour', CharityProject.close_date) -
+            extract('hour', CharityProject.create_date),
+        )
+    )
+    return projects.all()
 
 
 charity_project_crud = CRUDCharityProject(CharityProject)
